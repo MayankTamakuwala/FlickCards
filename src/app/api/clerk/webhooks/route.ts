7998a8eb22d14f06818124cd9292a3938d7f6cfe
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-08-16 11:17:54 -0400

    Update route.ts

--- src/app/api/clerk/webhooks/route.ts
+++ src/app/api/clerk/webhooks/route.ts
@@ -6,11 +6,11 @@ import { deleteDoc, doc, setDoc } from "firebase/firestore";
 require("dotenv").config();
 
 export async function POST(req: Request) {
-	const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+	const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
 	if (!WEBHOOK_SECRET) {
 		throw new Error(
-			"Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+			"Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
 		);
 	}
 
